371d15fc8e02b03c2d41d3a734262fc7def3ecee
Jason the noob <jason-ollivierre@hotmail.com>
2025-05-10 17:50:00 +0100

    pequeños cambios

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
-
 import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  prefetch: true,
+  server: {
+    allowedHosts: ['social-coins-lose.loca.lt']
+  },
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [icon()]
 });
